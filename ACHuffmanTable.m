1d4471bb0793b68219c24f229b5db60a14a051af
Louislar <a8945267@gmail.com>
2018-12-02 13:58:51 +0800

    讓huffman table變成一個2x2 cell array 比較方便, 並加上特例 EOB 16個0

--- ACHuffmanTable.m
+++ ACHuffmanTable.m
@@ -24,140 +24,144 @@ codewordTable={
     [1 1 1 1  1 1 1 1  1 1 1 0  1 0 1 1] [1 1 1 1  1 1 1 1  1 1 1 0  1 1 0 0] [1 1 1 1  1 1 1 1  1 1 1 0  1 1 0 1] [1 1 1 1  1 1 1 1  1 1 1 0  1 1 1 0] [1 1 1 1  1 1 1 1  1 1 1 0  1 1 1 1] [1 1 1 1  1 1 1 1  1 1 1 1  0 0 0 0] [1 1 1 1  1 1 1 1  1 1 1 1  0 0 0 1] [1 1 1 1  1 1 1 1  1 1 1 1  0 0 1 0] [1 1 1 1  1 1 1 1  1 1 1 1  0 0 1 1] [1 1 1 1  1 1 1 1  1 1 1 1  0 1 0 0];
     [1 1 1 1  1 1 1 1  1 1 1 1  0 1 0 1] [1 1 1 1  1 1 1 1  1 1 1 1  0 1 1 0] [1 1 1 1  1 1 1 1  1 1 1 1  0 1 1 1] [1 1 1 1  1 1 1 1  1 1 1 1  1 0 0 0] [1 1 1 1  1 1 1 1  1 1 1 1  1 0 0 1] [1 1 1 1  1 1 1 1  1 1 1 1  1 0 1 0] [1 1 1 1  1 1 1 1  1 1 1 1  1 0 1 1] [1 1 1 1  1 1 1 1  1 1 1 1  1 1 0 0] [1 1 1 1  1 1 1 1  1 1 1 1  1 1 0 1] [1 1 1 1  1 1 1 1  1 1 1 1  1 1 1 0]};
 
-if (run==0)
-    if (size==0)    %EOB end of block
-        runsizeCodeWord=[1 0 1 0];
-    end
-    if (size==1)  
-        runsizeCodeWord=[0 0];
-    end
-    if (size==2)
-        runsizeCodeWord=[0 1];
-    end
-    if (size==3)  
-        runsizeCodeWord=[1 0 0];
-    end
-    if (size==4)  
-        runsizeCodeWord=[1 0 1 1];
-    end
-    if (size==5)
-        runsizeCodeWord=[1 1 0 1 0];
-    end
-    if (size==6)
-        runsizeCodeWord=[1 1 1 1 0 0 0];
-    end
-    if (size==7)
-        runsizeCodeWord=[1 1 1 1 1 0 0 0];
-    end
-    if (size==8)
-        runsizeCodeWord=[1 1 1 1 1 1 0 1 1 0];
-    end
-    if (size==9)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 0];
-    end
-    if (size==10)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1];
-    end
-end
+% if (run==0)
+%     if (size==0)    %EOB end of block
+%         runsizeCodeWord=[1 0 1 0];
+%     end
+%     if (size==1)  
+%         runsizeCodeWord=[0 0];
+%     end
+%     if (size==2)
+%         runsizeCodeWord=[0 1];
+%     end
+%     if (size==3)  
+%         runsizeCodeWord=[1 0 0];
+%     end
+%     if (size==4)  
+%         runsizeCodeWord=[1 0 1 1];
+%     end
+%     if (size==5)
+%         runsizeCodeWord=[1 1 0 1 0];
+%     end
+%     if (size==6)
+%         runsizeCodeWord=[1 1 1 1 0 0 0];
+%     end
+%     if (size==7)
+%         runsizeCodeWord=[1 1 1 1 1 0 0 0];
+%     end
+%     if (size==8)
+%         runsizeCodeWord=[1 1 1 1 1 1 0 1 1 0];
+%     end
+%     if (size==9)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 0];
+%     end
+%     if (size==10)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1];
+%     end
+% end
+% 
+% if (run==1)
+%     if (size==1)    %EOB end of block
+%         runsizeCodeWord=[1 1 0 0];
+%     end
+%     if (size==2)
+%         runsizeCodeWord=[1 1 0 1 1];
+%     end
+%     if (size==3)  
+%         runsizeCodeWord=[1 1 1 1 0 0 1];
+%     end
+%     if (size==4)  
+%         runsizeCodeWord=[1 1 1 1 1 0 1 1 0];
+%     end
+%     if (size==5)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 0 1 1 0];
+%     end
+%     if (size==6)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 0 0];
+%     end
+%     if (size==7)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 0 1];
+%     end
+%     if (size==8)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 1 0];
+%     end
+%     if (size==9)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 1 1];
+%     end
+%     if (size==10)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 0 0];
+%     end
+% end
+% 
+% if (run==2)
+%     if (size==1)    %EOB end of block
+%         runsizeCodeWord=[1 1 1 0 0];
+%     end
+%     if (size==2)
+%         runsizeCodeWord=[1 1 1 1 1 0 0 1];
+%     end
+%     if (size==3)  
+%         runsizeCodeWord=[1 1 1 1 1 1 0 1 1 1];
+%     end
+%     if (size==4)  
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 0 1 0 0];
+%     end
+%     if (size==5)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 0 1];
+%     end
+%     if (size==6)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 1 0];
+%     end
+%     if (size==7)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 1 1];
+%     end
+%     if (size==8)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 1 0 0];
+%     end
+%     if (size==9)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 1 0 1];
+%     end
+%     if (size==10)
+%         runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 0];
+%     end
+% end
+% 
+% if (run==3)
+%     if (size==1)    %EOB end of block
+%         runsizeCodeWord=[1 1 1 0 1 0];
+%     end
+%     if (size==2)
+%         runsizeCodeWord=[1 1 1 1  1 0 1 1  1];
+%     end
+%     if (size==3)  
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  0 1 0 1];
+%     end
+%     if (size==4)  
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 0  1 1 1 1];
+%     end
+%     if (size==5)
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 0 0];
+%     end
+%     if (size==6)
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 0 1];
+%     end
+%     if (size==7)
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 1 0];
+%     end
+%     if (size==8)
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 1 1];
+%     end
+%     if (size==9)
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 1 0 0];
+%     end
+%     if (size==10)
+%         runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 1 0 1];
+%     end
+% end
 
-if (run==1)
-    if (size==1)    %EOB end of block
-        runsizeCodeWord=[1 1 0 0];
-    end
-    if (size==2)
-        runsizeCodeWord=[1 1 0 1 1];
-    end
-    if (size==3)  
-        runsizeCodeWord=[1 1 1 1 0 0 1];
-    end
-    if (size==4)  
-        runsizeCodeWord=[1 1 1 1 1 0 1 1 0];
-    end
-    if (size==5)
-        runsizeCodeWord=[1 1 1 1 1 1 1 0 1 1 0];
-    end
-    if (size==6)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 0 0];
-    end
-    if (size==7)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 0 1];
-    end
-    if (size==8)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 1 0];
-    end
-    if (size==9)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 0 1 1 1];
-    end
-    if (size==10)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 0 0];
-    end
-end
+%兩種特例 ---> run/size == 0/0  or  15/0
+%                         <EOB>     <16個0>
 
-if (run==2)
-    if (size==1)    %EOB end of block
-        runsizeCodeWord=[1 1 1 0 0];
-    end
-    if (size==2)
-        runsizeCodeWord=[1 1 1 1 1 0 0 1];
-    end
-    if (size==3)  
-        runsizeCodeWord=[1 1 1 1 1 1 0 1 1 1];
-    end
-    if (size==4)  
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 0 1 0 0];
-    end
-    if (size==5)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 0 1];
-    end
-    if (size==6)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 1 0];
-    end
-    if (size==7)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 0 1 1];
-    end
-    if (size==8)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 1 0 0];
-    end
-    if (size==9)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 1 0 1];
-    end
-    if (size==10)
-        runsizeCodeWord=[1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 0];
-    end
-end
-
-if (run==3)
-    if (size==1)    %EOB end of block
-        runsizeCodeWord=[1 1 1 0 1 0];
-    end
-    if (size==2)
-        runsizeCodeWord=[1 1 1 1  1 0 1 1  1];
-    end
-    if (size==3)  
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  0 1 0 1];
-    end
-    if (size==4)  
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 0  1 1 1 1];
-    end
-    if (size==5)
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 0 0];
-    end
-    if (size==6)
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 0 1];
-    end
-    if (size==7)
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 1 0];
-    end
-    if (size==8)
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 0 1 1];
-    end
-    if (size==9)
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 1 0 0];
-    end
-    if (size==10)
-        runsizeCodeWord=[1 1 1 1  1 1 1 1  1 0 0 1  0 1 0 1];
-    end
-end
-
+output=cell2mat(codewordTable(run, size));
 
 output=runsizeCodeWord;
